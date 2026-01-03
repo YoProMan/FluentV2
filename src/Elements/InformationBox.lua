271b163ddc8c6dd276c73aa8fc4a38463612ddb3
YoProMan <128311704+YoProMan@users.noreply.github.com>
2026-01-03 20:24:45 +0000

    Add InformationBox module for displaying information

---
+++ src/Elements/InformationBox.lua
@@ -0,0 +1,21 @@
+local Root = script.Parent.Parent
+local Components = Root.Components
+local Flipper = require(Root.Packages.Flipper)
+local Creator = require(Root.Creator)
+
+local InformationBox = {}
+InformationBox.__index = InformationBox
+InformationBox.__type = "InformationBox"
+
+function Paragraph:New(Config)
+	assert(Config.Title, "Paragraph - Missing Title")
+	Config.Content = Config.Content or ""
+
+	local InformationBox = require(Components.Element)(Config.Title, Config.Content, Paragraph.Container, false)
+	InformationBox.Frame.BackgroundTransparency = 1
+	InformationBox.Border.Transparency = 0.6
+
+	return InformationBox
+end
+
+return InformationBox
